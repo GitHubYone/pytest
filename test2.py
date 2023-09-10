43a497968ccf15bdae090d6450a6f5b297c67b27
GitHubYone <tkw_yoneta@outlook.com>
2023-09-10 18:55:36 +0900

    test2py+

---
+++ test2.py
@@ -0,0 +1,10 @@
+signal = input("Signal color? ")
+
+if signal == "red":
+    print("Stop!")
+elif signal == "yellow":
+    print("Slow down!")
+elif signal == "blue" or signal == "green":
+    print("Go!")
+else:
+    print("Invalid signal color...")
